2339d2cbafb09c6e4162cf40736702d9031a54b0
Markensau <oetkenlukas@gmail.com>
2022-01-29 13:34:21 +0100

    Creating Navbar; Creating Product OverView

--- index.php
+++ index.php
@@ -4,11 +4,55 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="css/styles.css">
+    <title>Lvcky Shop</title>
 </head>
 <body>
+    <div class="w3-row shoptextcontainer">
+        <div class="w3-col" style="width:20%">
+            <h2 class="shoptext">LvckyShop</h2>
+        </div>
+    </div>
     <?php
     include("mysql/mysql.php");
+
     ?>
+
+<div class="w3-row-padding w3-section w3-stretch">
+    <?php
+        // <div class="w3-col s4">
+        //     <img src="img_nature_wide.jpg" style="width:100%">
+        // </div>
+        $result = getProductList();
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                $id = $row["id"];
+                $productname = $row["productname"];
+                $description = $row["description"];
+                $price = $row["price"];
+                $imageURL = $row["imageURL"];
+                echo "<div class=\"w3-col s4\">";
+                echo "<div class=\"w3-card-4 w3-margin w3-grey\">";
+                echo "<img src=\"" . $imageURL . "\" alt=\"Nature\" style=\"width:100%\">";
+                echo "<div class=\"w3-container\">";
+                echo "<h3>$productname</h3>";
+                echo "<p>$description</p>";
+                echo "<p>$price</p>";
+                echo "<p>";
+                echo "<button class=\"w3-button w3-block w3-theme-l4\">Buy</button>";
+                echo "</p>";
+                echo "</div>";
+                echo "</div>";
+                echo "</div>";
+            
+            }
+        } else {
+            echo "Es wurden noch keine Produkte erstellt. Oder in der Datenbank gefunden.";
+        }
+    ?>
+
+</div>
 </body>
 </html>
--- mysql/mysql.php
+++ mysql/mysql.php
@@ -25,25 +25,12 @@ if ($conn->query($sql) === TRUE) {
   echo "Error creating table: " . $conn->error;
 }
 
-$sql = "CREATE TABLE IF NOT EXISTS products (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    productname VARCHAR(35) NOT NULL,
-    description VARCHAR(250) NOT NULL,
-    price INT(6) NOT NULL,
-    reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    )";
-    
-    if ($conn->query($sql) === TRUE) {
-      echo "<script>console.log('MySQL table created')</script>";
-    } else {
-      echo "Error creating table: " . $conn->error;
-    }
-
 
     $sql = "CREATE TABLE IF NOT EXISTS products (
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     productname VARCHAR(35) NOT NULL,
     description VARCHAR(250) NOT NULL,
+    imageURL LONGTEXT NOT NULL,
     price INT(6) NOT NULL,
     reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
     )";
@@ -53,14 +40,6 @@ $sql = "CREATE TABLE IF NOT EXISTS products (
     } else {
       echo "Error creating table: " . $conn->error;
     }
-
-    // $sql = "INSERT INTO `accounts` (`nickname`, `password`) VALUES ('lwroot', 'lwnewuser');";
-        
-    //     if ($conn->query($sql) === TRUE) {
-    //       echo "<script>console.log('MySQL table created')</script>";
-    //     } else {
-    //       echo "Error creating table: " . $conn->error;
-    //     }
 
 $conn->close();
 
@@ -83,5 +62,59 @@ function createNewDataBaseUser($nickname, $rawPassword) {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
     $conn->close();
+}
+
+function deleteUser($id) {
+    global $servername, $username, $dbname, $password;
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "DELETE FROM `accounts` WHERE  `id`=" . $id . ";";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>console.log('Deletetion Succesfull')</script>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+}
+
+function changePasswordFromUser($id, $newPassword) {
+    global $servername, $username, $dbname, $password;
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $hashedPassword = hash("sha256", $newPassword);
+
+    $sql = "UPDATE `accounts` SET `password`='" . $hashedPassword . "' WHERE  `id`=" . $id .";";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>console.log('New record created successfully')</script>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+    $conn->close();
+}
+
+function getProductList() {
+    global $servername, $username, $dbname, $password;
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT * FROM `products` LIMIT 1000;";
+
+    $result = $conn->query($sql);
+    return $result;
+    $conn->close();
+
 }
 ?>
