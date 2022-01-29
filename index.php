a8f2def60ba98ba7777ea1614ce82802e5e4b14e
Markensau <oetkenlukas@gmail.com>
2022-01-29 15:58:10 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,10 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="css/styles.css">
     <title>Lvcky Shop</title>
+    <!--- 
+    Start Server YEET
+    phpserver/php.exe -S localhost:80 
+    -->
 </head>
 <body>
     <div class="w3-row shoptextcontainer">
