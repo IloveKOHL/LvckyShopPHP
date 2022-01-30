e84424626b67347c11af650bb5861f59d6e1b699
Markensau <oetkenlukas@gmail.com>
2022-01-30 14:23:00 +0100

    More Security :apple:

--- mysql/mysql.php
+++ mysql/mysql.php
@@ -8,6 +8,7 @@ $password = "pwlvckyshop";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
+    session_destroy();
   die("Connection failed: " . $conn->connect_error);
 }
 
